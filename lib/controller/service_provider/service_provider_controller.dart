a96c8a05a56231f571ad51a2801157fffa775cb3
Hassan663 <hn893385@gmail.com>
2021-12-10 12:37:46 +0500

    service Provider auth

--- lib/controller/service_provider/service_provider_controller.dart
+++ lib/controller/service_provider/service_provider_controller.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ServiceProvider extends GetxController {
-
-  
   TextEditingController signupEmailController = TextEditingController();
   TextEditingController signupNameController = TextEditingController();
   TextEditingController signupPasswordController = TextEditingController();
@@ -14,27 +12,27 @@ class ServiceProvider extends GetxController {
 
   RxInt choise = 0.obs;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
+  // Future<bool> createAppoitmentInDatabase(Schedule schedule) async {
+  //   try {
+  //     Timestamp currentTime = Timestamp.now();
+  //     DocumentReference document = _firestore.collection('available-appointments').doc();
+  //     await document.set({
+  //       "nurseId": authController.currentUser.value.uid,
+  //       "schedule": schedule.toJson(),
+  //       "createdAt": Timestamp.now(),
+  //       "docId" : document.id
+  //     });
+  //     createAppointmentInNurseCollection(schedule, currentTime, document);
+  //     CustomSnackBar.showSnackBar(
+  //         title: "Appoitment successfully created",
+  //         message: '',
+  //         backgroundColor: snackBarSuccess);
+  //     return true;
+  //   } catch (e) {
+  //     debugPrint(e.toString());
+  //     return false;
+  //   }
+  // }
 
   // @override
   // void onInit() {
