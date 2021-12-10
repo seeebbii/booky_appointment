2d394d8be99232f3069ac9671c81c91d6dd05aca
Hassan663 <hn893385@gmail.com>
2021-12-10 11:20:02 +0500

    firestore services

---
+++ lib/controller/service_provider/service_provider_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class ServiceProvider extends GetxController {
+
+
+  TextEditingController signupEmailController = TextEditingController();
+  TextEditingController signupNameController = TextEditingController();
+  TextEditingController signupPasswordController = TextEditingController();
+  TextEditingController signupPhoneController = TextEditingController();
+  TextEditingController signupBusinessNameController = TextEditingController();
+
+
+
+
+  // RxInt coins = 0.obs;
+  // var optionSelected = false.obs;
+  // var productSelected = false.obs;
+
+  // void getProductList() async {
+  //   productModel.value = await ApiClient.productService.getProduct();
+  // }
+
+  // @override
+  // void onInit() {
+  //   getProductList();
+  //   super.onInit();
+  // }
+}
