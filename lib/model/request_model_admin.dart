0cb3d7014b5156ac000986167c3843c3d09a9490
Hassan663 <hn893385@gmail.com>
2021-12-10 18:33:04 +0500

    shops

--- lib/model/request_model_admin.dart
+++ lib/model/request_model_admin.dart
@@ -3,36 +3,27 @@ import 'package:booky/model/provider_business_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class RequestModelAdmin{
+class RequestModelAdmin {
   String? docid;
- ProviderBusinessModel ?shop;
- AuthModel ?user;
-
-
-  
+  ProviderBusinessModel? shop;
+  AuthModel? user;
 
   RequestModelAdmin({
     this.docid,
     this.shop,
     this.user,
-    
   });
 
   RequestModelAdmin.fromDocumentSnapshot(Map<String, dynamic> doc) {
     try {
-    docid=doc['docid'];
-    shop=doc['shop'];
-    user=doc['detail'];
-      
+      docid = doc['docid'];
+      shop = doc['shop'];
+      user = doc['detail'];
+    } catch (e) {}
+    Map<String, dynamic> toJson() => {
+          'docid': doc,
+          'shop': shop,
+          'providerdetail': user,
+        };
   }
-  catch (e)
-  {
-
-  }
-  Map<String, dynamic> toJson() => {
-    'docid':doc,
-      'shop': shop,
-      'providerdetail':user,
-      };
-}
 }
