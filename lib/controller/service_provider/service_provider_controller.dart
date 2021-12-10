927c9e6b6e83ef2de88eea69e76c0414eed0afd4
Hassan663 <hn893385@gmail.com>
2021-12-10 12:40:01 +0500

    service Provider auth

--- lib/controller/service_provider/service_provider_controller.dart
+++ lib/controller/service_provider/service_provider_controller.dart
@@ -1,13 +1,19 @@
+import 'package:booky/controller/authentication/auth_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ServiceProvider extends GetxController {
+
+
+ final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final authController = Get.find<AuthController>();
+
   TextEditingController signupEmailController = TextEditingController();
   TextEditingController signupNameController = TextEditingController();
   TextEditingController signupPasswordController = TextEditingController();
   TextEditingController signupPhoneController = TextEditingController();
   TextEditingController signupBusinessNameController = TextEditingController();
-
   TextEditingController locationController = TextEditingController();
 
   RxInt choise = 0.obs;
